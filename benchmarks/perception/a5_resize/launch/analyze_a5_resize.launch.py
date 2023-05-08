1169ce7d14b1cf72141873f6569a11ee4e445740
Martiño Crespo <martinho@accelerationrobotics.com>
2023-05-08 11:08:50 +0200

    address PR feedback

--- benchmarks/perception/a5_resize/launch/analyze_a5_resize.launch.py
+++ benchmarks/perception/a5_resize/launch/analyze_a5_resize.launch.py
@@ -4,7 +4,7 @@
 #    @@@@@ @@  @@    @@@@
 #    @@@@@ @@  @@    @@@@
 #    @@@@@ @@  @@    @@@@ Copyright (c) 2023, Acceleration Robotics®
-#    @@@@@ @@  @@    @@@@ Author: Víctor Mayoral Vilches <victor@accelerationrobotics.com>
+#    @@@@@ @@  @@    @@@@ Author: Martiño Crespo <martinho@accelerationrobotics.com>
 #    @@@@@ @@  @@    @@@@
 #    @@@@@@@@@&@@@@@@@@@@
 #    @@@@@@@@@@@@@@@@@@@@
