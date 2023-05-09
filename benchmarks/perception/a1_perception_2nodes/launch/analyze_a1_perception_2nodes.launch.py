aa1b778b2b52c1d35c250f0826bf9a017747f773
Martiño Crespo <martinho@accelerationrobotics.com>
2023-05-09 12:55:09 +0200

    modify a1 analysis script authors

--- benchmarks/perception/a1_perception_2nodes/launch/analyze_a1_perception_2nodes.launch.py
+++ benchmarks/perception/a1_perception_2nodes/launch/analyze_a1_perception_2nodes.launch.py
@@ -4,6 +4,7 @@
 #    @@@@@ @@  @@    @@@@
 #    @@@@@ @@  @@    @@@@
 #    @@@@@ @@  @@    @@@@ Copyright (c) 2023, Acceleration Robotics®
+#    @@@@@ @@  @@    @@@@ Author: Víctor Mayoral Vilches <victor@accelerationrobotics.com>
 #    @@@@@ @@  @@    @@@@ Author: Martiño Crespo <martinho@accelerationrobotics.com>
 #    @@@@@ @@  @@    @@@@
 #    @@@@@@@@@&@@@@@@@@@@
@@ -39,18 +40,18 @@ ba.add_target({"name": "ros2:callback_start", "name_disambiguous": "ros2:callbac
 ba.add_target({"name": "ros2_image_pipeline:image_proc_rectify_cb_init", "name_disambiguous": "ros2_image_pipeline:image_proc_rectify_cb_init", "colors_fg": "yellow", "colors_fg_bokeh": "salmon", "layer": "userland", "label_layer": 4, "marker": "plus"})
 ba.add_target({"name": "ros2_image_pipeline:image_proc_rectify_init", "name_disambiguous": "ros2_image_pipeline:image_proc_rectify_init", "colors_fg": "red", "colors_fg_bokeh": "darksalmon", "layer": "userland", "label_layer": 4, "marker": "plus"})
 ba.add_target({"name": "ros2_image_pipeline:image_proc_rectify_fini", "name_disambiguous": "ros2_image_pipeline:image_proc_rectify_fini", "colors_fg": "red", "colors_fg_bokeh": "lightcoral", "layer": "userland", "label_layer": 4, "marker": "plus"})
-#ba.add_target({"name": "ros2:rclcpp_publish", "name_disambiguous": "ros2:rclcpp_publish", "colors_fg": "blue", "colors_fg_bokeh": "indianred", "layer": "rclcpp", "label_layer": 3, "marker": "plus"})
-#ba.add_target({"name": "ros2:rcl_publish", "name_disambiguous": "ros2:rcl_publish", "colors_fg": "blue", "colors_fg_bokeh": "crimson" , "layer": "rcl", "label_layer": 2, "marker": "plus"})
-#ba.add_target({"name": "ros2:rmw_publish", "name_disambiguous": "ros2:rmw_publish", "colors_fg": "blue", "colors_fg_bokeh": "firebrick" , "layer": "rmw", "label_layer": 1, "marker": "plus"})
+# ba.add_target({"name": "ros2:rclcpp_publish", "name_disambiguous": "ros2:rclcpp_publish", "colors_fg": "blue", "colors_fg_bokeh": "indianred", "layer": "rclcpp", "label_layer": 3, "marker": "plus"})
+# ba.add_target({"name": "ros2:rcl_publish", "name_disambiguous": "ros2:rcl_publish", "colors_fg": "blue", "colors_fg_bokeh": "crimson" , "layer": "rcl", "label_layer": 2, "marker": "plus"})
+# ba.add_target({"name": "ros2:rmw_publish", "name_disambiguous": "ros2:rmw_publish", "colors_fg": "blue", "colors_fg_bokeh": "firebrick" , "layer": "rmw", "label_layer": 1, "marker": "plus"})
 ba.add_target({"name": "ros2_image_pipeline:image_proc_rectify_cb_fini", "name_disambiguous": "ros2_image_pipeline:image_proc_rectify_cb_fini", "colors_fg": "yellow", "colors_fg_bokeh": "darkred", "layer": "userland", "label_layer": 4, "marker": "plus"})
 ba.add_target({"name": "ros2:callback_end", "name_disambiguous": "ros2:callback_end (2)", "colors_fg": "blue", "colors_fg_bokeh": "red", "layer": "rclcpp", "label_layer": 3, "marker": "diamond"})
 ba.add_target({"name": "ros2:callback_start", "name_disambiguous": "ros2:callback_start (3)", "colors_fg": "blue", "colors_fg_bokeh": "lavender", "layer": "rclcpp", "label_layer": 3, "marker": "diamond"})
 ba.add_target({"name": "ros2_image_pipeline:image_proc_resize_cb_init", "name_disambiguous": "ros2_image_pipeline:image_proc_resize_cb_init", "colors_fg": "yellow", "colors_fg_bokeh": "thistle", "layer": "userland", "label_layer": 4, "marker": "plus"})
 ba.add_target({"name": "ros2_image_pipeline:image_proc_resize_init", "name_disambiguous": "ros2_image_pipeline:image_proc_resize_init", "colors_fg": "red", "colors_fg_bokeh": "plum", "layer": "userland", "label_layer": 4, "marker": "plus"})
 ba.add_target({"name": "ros2_image_pipeline:image_proc_resize_fini", "name_disambiguous": "ros2_image_pipeline:image_proc_resize_fini", "colors_fg": "red", "colors_fg_bokeh": "fuchsia", "layer": "userland", "label_layer": 4, "marker": "plus"})
-#ba.add_target({"name": "ros2:rclcpp_publish", "name_disambiguous": "ros2:rclcpp_publish (2)", "colors_fg": "blue", "colors_fg_bokeh": "mediumorchid", "layer": "rclcpp", "label_layer": 3, "marker": "plus"})
-#ba.add_target({"name": "ros2:rcl_publish", "name_disambiguous": "ros2:rcl_publish (2)", "colors_fg": "blue", "colors_fg_bokeh": "mediumpurple", "layer": "rcl", "label_layer": 2, "marker": "plus"})
-#ba.add_target({"name": "ros2:rmw_publish", "name_disambiguous": "ros2:rmw_publish (2)", "colors_fg": "blue", "colors_fg_bokeh": "darkmagenta", "layer": "rmw", "label_layer": 1, "marker": "plus"})
+# ba.add_target({"name": "ros2:rclcpp_publish", "name_disambiguous": "ros2:rclcpp_publish (2)", "colors_fg": "blue", "colors_fg_bokeh": "mediumorchid", "layer": "rclcpp", "label_layer": 3, "marker": "plus"})
+# ba.add_target({"name": "ros2:rcl_publish", "name_disambiguous": "ros2:rcl_publish (2)", "colors_fg": "blue", "colors_fg_bokeh": "mediumpurple", "layer": "rcl", "label_layer": 2, "marker": "plus"})
+# ba.add_target({"name": "ros2:rmw_publish", "name_disambiguous": "ros2:rmw_publish (2)", "colors_fg": "blue", "colors_fg_bokeh": "darkmagenta", "layer": "rmw", "label_layer": 1, "marker": "plus"})
 ba.add_target({"name": "ros2_image_pipeline:image_proc_resize_cb_fini", "name_disambiguous": "ros2_image_pipeline:image_proc_resize_cb_fini", "colors_fg": "yellow", "colors_fg_bokeh": "indigo", "layer": "userland", "label_layer": 4, "marker": "plus"})
 ba.add_target({"name": "ros2:callback_end", "name_disambiguous": "ros2:callback_end (3)", "colors_fg": "blue", "colors_fg_bokeh": "mediumslateblue", "layer": "rclcpp", "label_layer": 3, "marker": "diamond"})
 ba.add_target({"name": "ros2:callback_start", "name_disambiguous": "ros2:callback_start (4)", "colors_fg": "blue", "colors_fg_bokeh": "chartreuse", "layer": "rclcpp", "label_layer": 3, "marker": "diamond"})
