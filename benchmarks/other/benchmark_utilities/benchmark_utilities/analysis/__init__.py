bf776f4e08e06bf0aa432f59899b36246bc6b9f0
Martiño Crespo <martinho@accelerationrobotics.com>
2023-09-15 11:47:13 +0200

    Output target_chain samples to markdown table results
    Signed-off-by: Martiño Crespo <martinho@accelerationrobotics.com>

--- benchmarks/other/benchmark_utilities/benchmark_utilities/analysis/__init__.py
+++ benchmarks/other/benchmark_utilities/benchmark_utilities/analysis/__init__.py
@@ -2287,6 +2287,10 @@ class BenchmarkAnalyzer:
             list_statistics[1].append("---")
             list_statistics[2].append((self.get_time_spent_in_target_chain()))
 
+            list_statistics[0].append("Samples")
+            list_statistics[1].append("---")
+            list_statistics[2].append("{}".format(self.get_target_chain_samples()))
+
             list_statistics[0].append("Lost Messages")
             list_statistics[1].append("---")
             list_statistics[2].append("{:.2f} %".format((self.lost_msgs/len(self.image_pipeline_msg_sets))*100))
@@ -3139,16 +3143,9 @@ class BenchmarkAnalyzer:
             print("Type {} for filtering power trace sets does not exist, setting message ID analysis type".format(filter_type))
             self.power_trace_sets_filter_type = "ID"
 
-    def get_time_spent_in_target_chain(self, tracepath=None, debug=True):
+    def get_time_spent_in_target_chain(self):
         """
         Compute the amount of time during which the traces of a specific target_chain were being generated
-
-        Args:
-            tracepath (string, optional):
-                Path of the CTF tracefiles. Defaults to None.
-
-            debug (string, optional):
-                Weather to print debugging information.
         """
 
         testing_time_ns = 0.0
@@ -3156,6 +3153,14 @@ class BenchmarkAnalyzer:
             testing_time_ns += (msg_set[-1].default_clock_snapshot.ns_from_origin - msg_set[0].default_clock_snapshot.ns_from_origin)
 
         return testing_time_ns/1e6
+
+    def get_target_chain_samples(self):
+        """
+        Compute the amount of times the target_chain was generated
+        """
+
+        return len(self.image_pipeline_msg_sets)
+
 
     def get_time_spent_in_specified_target_chains(self, trace_path, target_chain_name):
         """
