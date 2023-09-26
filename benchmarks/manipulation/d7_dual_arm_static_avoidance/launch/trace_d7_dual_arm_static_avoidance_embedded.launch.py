2fae77de97b19cffa28c988a30506bdbc679940d
Martiño Crespo <martinho@accelerationrobotics.com>
2023-09-26 17:52:53 +0200

    Separate d7 launch files
    Signed-off-by: Martiño Crespo <martinho@accelerationrobotics.com>

---
+++ benchmarks/manipulation/d7_dual_arm_static_avoidance/launch/trace_d7_dual_arm_static_avoidance_embedded.launch.py
@@ -0,0 +1,49 @@
+#
+#    @@@@@@@@@@@@@@@@@@@@
+#    @@@@@@@@@&@@@&&@@@@@
+#    @@@@@ @@  @@    @@@@
+#    @@@@@ @@  @@    @@@@
+#    @@@@@ @@  @@    @@@@ Copyright (c) 2023, Acceleration Robotics®
+#    @@@@@ @@  @@    @@@@ Author: Víctor Mayoral Vilches <victor@accelerationrobotics.com>
+#    @@@@@ @@  @@    @@@@ Author: Alejandra Martínez Fariña <alex@accelerationrobotics.com>
+#    @@@@@ @@  @@    @@@@ Author: Martiño Crespo Álvarez <martinho@accelerationrobotics.com>
+#    @@@@@ @@  @@    @@@@
+#    @@@@@@@@@&@@@@@@@@@@
+#    @@@@@@@@@@@@@@@@@@@@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    d7_embedded_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('robotcore_manipulation_moveit2'),
+                'launch',
+                'simulation',
+                'd7_simulation_moveit2_embedded.launch.py'
+            ])
+        ])
+    )
+
+    return LaunchDescription([
+        d7_embedded_launch
+    ])
